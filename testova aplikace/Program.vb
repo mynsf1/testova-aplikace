741c215c4e6e0b204c5dd0a4527eac60624a7300
Rubchevska Kateryna <Rubchevska.Kateryna@stud.vassboskovice.cz>
2024-01-31 08:22:48 +0100

    Přidejte soubory projektu.

---
+++ testova aplikace/Program.vb
@@ -0,0 +1,14 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.Clear()
+        Console.BackgroundColor = ConsoleColor.Cyan
+        Console.ForegroundColor = ConsoleColor.DarkCyan
+        Console.WriteLine("    
+
+      
+                   uwu uwu meow")
+        Console.Clear()
+    End Sub
+End Module
